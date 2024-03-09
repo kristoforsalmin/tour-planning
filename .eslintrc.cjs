e0a06ff850ceee67a5f788940cce8eb6f8f55026
Kristofor Salmin <4795426+kristoforsalmin@users.noreply.github.com>
2024-03-09 16:43:51 +0100

    Update auxiliary files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,20 @@
 /* eslint-env node */
+
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
+  },
+  rules: {
+    'comma-dangle': ['error', 'never'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never']
   }
 }
